a7d24bb0916cf95ae86734d057a299e2ef4577c4
Mass1milian0 <maxymino@hotmail.it>
2021-10-26 11:14:34 +0100

    backend is ready to roll
    backend is pretty much ready
    we got a few more dependencies
    also we got websocket support
    i want frontend to get and pass info from websocket

--- server.js
+++ server.js
@@ -2,21 +2,32 @@
 require('dotenv').config()
 const fastify = require('fastify')({ logger: true })
 const path = require('path');
-
+const mysql = require('mysql')
 const port = process.env.PORT || 3000
+fastify.register(require('fastify-websocket'))
+
+const connection = mysql.createConnection({
+    host: "localhost:3306",
+    user: "root",
+    password: process.env.DB_PASSWD
+})
+
+connection.connect((err) =>{
+    if (err) throw err
+    console.log("Connected!")
+})
 
 fastify.register(require('fastify-static'), {
     root: path.join(__dirname, 'public'),
     prefix: '/public/', // optional: default '/'
   })
 
-// Declare a route
+
 fastify.get('/public', function (req, reply) {
     return reply.sendFile('index.html')
   })
   
 
-// Run the server!
 const start = async () => {
   try {
     await fastify.listen(port)
