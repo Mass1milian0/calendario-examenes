a87a1d00f3d101a097583a7790e17f42efb87b3b
Alexandru Robert Lazar <alex2000robert@yahoo.com>
2021-10-30 23:40:05 +0100

    Add files via upload

---
+++ components/CheckBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label :for="id">{{shownName}}</label>
+    <input
+    :id="id"
+    type="checkbox"
+    :form="formId"
+    :name="name"
+    :value="value"
+    class="border-2 border-black rounded-md">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckBox',
+  props: {
+    formId: String,
+    id: String,
+    name: String,
+    value: String,
+    shownName: String
+  }
+}
+</script>
