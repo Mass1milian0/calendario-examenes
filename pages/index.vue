512657d3915606637216a95b764a2a172abea054
Mass1milian0 <maxymino@hotmail.it>
2021-11-23 12:14:53 +0000

    no se fixeaaaaaaa

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,111 @@
 <template>
-<client-only>
   <div class="w-screen flex lg:flex-row flex-col">
-    <div class="lg:w-3/5 w-full lg:my-auto border-2 border-black lg:mx-16 my-10">
-      <Calendario />
+    <div
+      class="lg:w-3/5 w-full lg:my-auto border-2 border-black lg:mx-16 my-10"
+    >
+      <client-only placeholder="loading...">
+        <Calendario />
+      </client-only>
     </div>
-    <div class="lg:w-1/5 w-full lg:my-16 mt-10 border-2 border-black lg:ml-16 h-full lg:py-16">
+    <div
+      class="
+        lg:w-1/5
+        w-full
+        lg:my-16
+        mt-10
+        border-2 border-black
+        lg:ml-16
+        h-full
+        lg:py-16
+      "
+    >
       <form action="#" @submit.prevent="updateData" id="formulario-main">
         <div class="flex flex-col justify-around h-full px-3">
           <div class="flex flex-col">
-            <Selector :options="universidadOptions" formId="formulario-main" id="universidad-options" name="universidad" placeholder="Seleccione universidad" />
-            <Selector :options="carreraOptions" formId="formulario-main" id="universidad-options" name="universidad" placeholder="Seleccione carrera" />
-            <Selector :options="cursoOptions" formId="formulario-main" id="universidad-options" name="universidad" placeholder="Seleccione curso" />
+            <client-only placeholder="loading...">
+              <Selector
+                :options="universidadOptions"
+                formId="formulario-main"
+                id="universidad-options"
+                name="universidad"
+                placeholder="Seleccione universidad"
+              />
+            </client-only>
+            <client-only placeholder="loading...">
+              <Selector
+                :options="carreraOptions"
+                formId="formulario-main"
+                id="universidad-options"
+                name="universidad"
+                placeholder="Seleccione carrera"
+              />
+            </client-only>
+            <client-only placeholder="loading...">
+              <Selector
+                :options="cursoOptions"
+                formId="formulario-main"
+                id="universidad-options"
+                name="universidad"
+                placeholder="Seleccione curso"
+              />
+            </client-only>
           </div>
           <div class="flex flex-col font-bold lg:mt-96">
-            <CheckBox formId="formulario-main" id="conv-extraordinaria" name="conv" value="ext" shownName="Extraordinaria" />
-            <CheckBox formId="formulario-main" id="conv-especial" name="conv" value="esp" shownName="Especial" />
+            <client-only placeholder="loading...">
+              <CheckBox
+                formId="formulario-main"
+                id="conv-extraordinaria"
+                name="conv"
+                value="ext"
+                shownName="Extraordinaria"
+              />
+            </client-only>
+            <client-only placeholder="loading...">
+              <CheckBox
+                formId="formulario-main"
+                id="conv-especial"
+                name="conv"
+                value="esp"
+                shownName="Especial"
+              />
+            </client-only>
           </div>
-          </div>
+        </div>
       </form>
     </div>
   </div>
-  </client-only>
 </template>
 
 
 <script>
-const HOST = document.location.origin //FIXME cosa de alex
-import Calendario from '~/components/Calendar.vue'
-import CheckBox from '~/components/CheckBox.vue'
-import Selector from '~/components/Selector.vue'
+const HOST = document.location.origin //FIXME no funciona nada, no se fixea el puto jodio
 export default {
-  components: { Calendario, CheckBox, Selector },
+  components: { 
+    Calendario: () => import("@/components-lazy/Calendar.vue"), 
+    CheckBox: () => import("@/components-lazy/CheckBox.vue"), 
+    Selector: () => import("@/components-lazy/CheckBox.vue")
+    },
   computed: {
-    async universidadOptions () {
-      let options = []
-      //response = await fetch(HOST + "/api/universidades")
-      //console.log(response)
-      return options
+    async universidadOptions() {
+      let options = [];
+      response = await fetch(HOST + "/api/universidades")
+      console.log(response)
+      return options;
     },
-    carreraOptions () {
+    carreraOptions() {
       // Cambiar const por let.
-      const options = []
+      const options = [];
       // Hacer cosas en la API para sacar las opciones de carreras.
-      return options
+      return options;
     },
-    cursoOptions () {
-      return [1,2,3,4,5]
-    }
+    cursoOptions() {
+      return [1, 2, 3, 4, 5];
+    },
   },
   methods: {
-    updateData (data) {
+    updateData(data) {
       // Hacer llamada a API.
-    }
-  }
-}
+    },
+  },
+};
 </script>
